6b4497846bb1dc55ac4573c14a4f3a96cef5210d
Meliousas <meliousas@gmail.com>
2016-11-15 22:49:42 +0100

    Profile and Account classes added

---
+++ src/main/java/Profile.java
@@ -0,0 +1,56 @@
+/**
+ * Created by Melious on 15.11.2016.
+ */
+public class Profile {
+
+    private int age;
+    private String firstName;
+    private String lastName;
+    private String city;
+    private String country;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+
+
+
+
+}
